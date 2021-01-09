97f1e268881274256198c1586203b3fdb98af9d2
Sehui Jang <hee990605@naver.com>
2021-01-10 02:16:03 +0900

    [#12] enhancement : 사용자 얼굴 인식

--- frontend/camera.py
+++ frontend/camera.py
@@ -1,5 +1,6 @@
 import cv2
- 
+
+
 class VideoCamera(object):
     def __init__(self):
         self.video = cv2.VideoCapture(0)
@@ -9,7 +10,17 @@ class VideoCamera(object):
 
     def get_frame(self):
         success, image = self.video.read()
+        image_gray = cv2.cvtColor(image,cv2.COLOR_BGR2GRAY)
+        face_cascade = cv2.CascadeClassifier('haarcascade_frontalface_default.xml')
+        faces=face_cascade.detectMultiScale(image_gray, 1.3, 5)
+
+        for (x,y,w,h) in faces:
+            cv2.rectangle(image, (x,y),(x+w,y+h),(255,0,0),2)
+            roi_gray=image_gray[y:y+h, x:x+w]
+            roi_color=image[y:y+h, x:x+w]
+
         ret, jpeg = cv2.imencode('.jpg', image)
+
         return jpeg.tobytes()
 
 
