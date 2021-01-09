96d5c80de9dfe1be9b66e6574cc408e063da0d68
Sehui Jang <hee990605@naver.com>
2021-01-10 05:57:13 +0900

    [#14] enhancement : 자세 불안정 시 경고

--- frontend/camera.py
+++ frontend/camera.py
@@ -1,5 +1,7 @@
 import cv2
- 
+import winsound as sd
+
+
 class VideoCamera(object):
     def __init__(self):
         self.video = cv2.VideoCapture(0)
@@ -9,7 +11,21 @@ class VideoCamera(object):
 
     def get_frame(self):
         success, image = self.video.read()
+        image_gray = cv2.cvtColor(image,cv2.COLOR_BGR2GRAY)
+        face_cascade = cv2.CascadeClassifier('haarcascade_frontalface_default.xml')
+        faces=face_cascade.detectMultiScale(image_gray, 1.3, 5)
+
+        for (x,y,w,h) in faces:
+            cv2.rectangle(image, (x,y),(x+w,y+h),(255,0,0),2)
+            roi_gray=image_gray[y:y+h, x:x+w]
+            roi_color=image[y:y+h, x:x+w]
+            if (w > 260 or h > 260):
+                fr = 2000
+                du = 1000
+                sd.Beep(fr, du)
+
         ret, jpeg = cv2.imencode('.jpg', image)
+
         return jpeg.tobytes()
 
 
